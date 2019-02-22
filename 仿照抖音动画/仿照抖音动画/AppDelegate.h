63ea2ba35bd425dd4845d5f9900e849ca47cf0f0
Apple <apple@apples-macbook-pro.local>
2019-02-22 20:53:17 +0800

    上传项目

---
+++ 仿照抖音动画/仿照抖音动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿照抖音动画
+//
+//  Created by Apple on 22/2/19.
+//  Copyright © 2019年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿照抖音动画/仿照抖音动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿照抖音动画
+//
+//  Created by Apple on 22/2/19.
+//  Copyright © 2019年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
